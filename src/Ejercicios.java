a989875eb98a15eda552dc898a23b555fdb360bd
Anabel <anabelmd@outlook.es>
2023-05-25 13:28:50 +0200

    Primer método creado: devolver número aleatorio

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -1,4 +1,14 @@
 
 public class Ejercicios {
+	/*
+	 * 1.1 Método que devuelva un número aleatorio entre dos pasados por argumentos
+	 * del método.
+	 */
 
+	public int numeroAleatorio(int numeroMax, int numeroMin) {
+		int resultado = 0;
+
+		resultado = (int) (Math.random() * (numeroMax - numeroMin + 1) + numeroMin);
+		return resultado;
+	}
 }
