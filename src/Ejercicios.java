8052e74ac7c777ac6746c562fbfe16cb1a3060ee
Anabel <anabelmd@outlook.es>
2023-05-25 13:40:55 +0200

    Creada la clase persona del ejercicio 1.2 del bloque 1

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -11,4 +11,69 @@ public class Ejercicios {
 		resultado = (int) (Math.random() * (numeroMax - numeroMin + 1) + numeroMin);
 		return resultado;
 	}
+
+	/*
+	 * 1.2 Diseñe un método que imprima los datos de una persona ingresados por
+	 * teclado e indicar si es mayor o menor de edad. El método recibe una clase
+	 * Persona que tiene nombre y edad; crea la clase necesaria. Es mayor de edad si
+	 * tiene 18 o más.
+	 */
+	
+	public void imprimirDatosPersona(Persona p) {
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	class Persona{
+		private String nombre;
+		private int edad;
+		
+		public Persona(String nombre, int edad) {
+			this.nombre = nombre;
+			this.edad = edad;
+		}
+
+		/**
+		 * @return the nombre
+		 */
+		public String getNombre() {
+			return nombre;
+		}
+
+		/**
+		 * @param nombre the nombre to set
+		 */
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		/**
+		 * @return the edad
+		 */
+		public int getEdad() {
+			return edad;
+		}
+
+		/**
+		 * @param edad the edad to set
+		 */
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+
+		@Override
+		public String toString() {
+			return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+		}
+		
+		
+		
+		
+	}
 }
